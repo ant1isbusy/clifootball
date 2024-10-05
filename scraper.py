0f78c197bb1e780ed414551e1e089973f04c617a
Anto Dovodja <anto.dovodja@student.tugraz.at>
2024-10-05 18:01:44 +0200

    step by step

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 
 import re # regex
 import requests
+import time
 
 import json
 import pandas as pd
@@ -18,10 +19,11 @@ class Player:
         self.curr_team = curr
 
 def getRawJsonPlayer(ID):
+    print("Loading data ...")
     response = requests.get("https://understat.com/player/" + ID)
+
     if response.status_code != 200:
         print("Client Error!")
-
     
     html = BS(response.content, "html.parser")
 
@@ -52,19 +54,43 @@ def getRawJsonPlayer(ID):
     data = json.loads(shotsData.encode('utf8').decode('unicode_escape'))
     return data, selected_player
 
-def pandas_query(data):
+def pandas_query(data, player):
     df = pd.DataFrame(data)
+
+    # printAvailable seasons:
+    print("")
+    for i in range (len(player.teams)):
+        print("(" + str((i + 1)) + ") " + player.teams[i][0] + "/" + str(int(player.teams[i][0]) + 1) + " - " + player.teams[i][1]) 
+
     season_selected = 0
-    print(df.iloc[0])
+    while True:
+        temp = input("Select season to analyze: ")
+        if temp.isdigit():
+            season_selected = int(temp) - 1
+            if 0 <= season_selected < len(player.teams):
+                break
+            else:
+                print(f"Please enter a number between 1 and {len(player.teams)}.")
+        else:
+            print("Invalid input. Please enter a number.")
+    
+    season = player.teams[season_selected][0]
+
+    season_df = df[df["season"] == season]
+    print(season_df.columns)
+
+    
+    goals = season_df[season_df["result"] == "Goal"]
+    print(goals)
 
 def scrapePlayer(ID):
     json_data, player = getRawJsonPlayer(ID)
-    print(player.curr_team)
-    pandas_query(json_data)
+    print("Current team: " + red + player.curr_team + reset)
+    pandas_query(json_data, player)
 
 # TODO: https://www.footballfancast.com/premier-league-stadims-pitch-sizes-ranked-biggest-smallest/
     # take the different pitchsizes into consideration, not all pitches are of the same size in the EPL
 
     # in the top 5 leagues -> calculate pitch size, based on that, the calculations of X,Y shall happen, have a hashmap: league -> avg pitch dimensions.
     
-
+    # take defenders into account, or players which do not have any goals or shots, then theirshotmap is empty
